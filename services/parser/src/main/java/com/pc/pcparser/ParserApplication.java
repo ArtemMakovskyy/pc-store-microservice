6a02daec2a021da1ffa1831bd2b4d0020a22dd54
Artem Makovskyy <photomak79@gmail.com>
2025-02-18 21:42:11 +0200

    create parser

--- services/parser/src/main/java/com/pc/pcparser/ParserApplication.java
+++ services/parser/src/main/java/com/pc/pcparser/ParserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PcParserApplication {
+public class ParserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PcParserApplication.class, args);
+		SpringApplication.run(ParserApplication.class, args);
 	}
 
 }
